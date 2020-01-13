130fe8e499af7e92678b8b6a1e2e68dd5068393e
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-01-13 16:19:38 -0300

    Fixed long line

--- planck.py
+++ planck.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from scipy.constants import h, c, k, pi
 
 
-def planck(wavelengths, temperature):
-    rho = (8 * pi * h * c) / (wavelengths**5 *
-                              (np.exp((h * c) / (wavelengths * k * temperature)) - 1))
+def planck(wavelength, temperature):
+    rho = (8 * pi * h * c) / (wavelength**5 * (np.exp((h * c) /
+                                                      (wavelength * k *
+                                                       temperature)) - 1))
     return rho
